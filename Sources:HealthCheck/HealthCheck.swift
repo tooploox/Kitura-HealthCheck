28514b64ddadc0bc1221742ee295584d85e4d9a6
Jeremi Kaczmarczyk <jeremi@tooploox.com>
2016-06-24 13:22:34 +0200

    Add HealthCheck class

---
+++ Sources:HealthCheck/HealthCheck.swift
@@ -0,0 +1,20 @@
+//
+//  HealthCheck.swift
+//  
+//
+//  Created by Jeremi Kaczmarczyk on 24.06.2016.
+//
+//
+
+import Foundation
+import Kitura
+import HeliumLogger
+import LoggerAPI
+
+class HealthCheck: RouterMiddleware {
+    
+    func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) throws {
+        try response.end()
+        Log.info("Health check performed. Status: OK.")
+    }
+}
